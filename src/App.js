8d4d62c2eb56bdf5fabb702ee5a56ce3623d9651
farhan0651 <fk912254@gmail.com>
2021-05-21 21:47:02 +0530

    added the functionality to keep track of history of moves

--- src/App.js
+++ src/App.js
@@ -5,28 +5,35 @@ import { calculateWinner } from "./winnerLogic";
 
 const App=()=>{
 
-    const [board,setBoard]=useState(Array(9).fill(null))
-    const [nextPlayer,seNextPlayer]=useState(false);
-    const winner=calculateWinner(board);
+    // const [board,setBoard]=useState(Array(9).fill(null))
+    // const [nextPlayer,seNextPlayer]=useState(false);
+    const [history,setHistory]=useState([{board:Array(9).fill(null),isNext: true}])
+    const [currentMove,setCurrentMove]=useState(0);
+    const current=history[currentMove];
+    const winner=calculateWinner(current.board);
+    console.log('history',history);  
 
     const message= winner 
         ? `Winner is ${winner}` 
-        : `Next player is ${nextPlayer ? 'X':'O'}`; 
+        : `Next player is ${current.isNext ? 'X':'O'}`; 
 
     const hadnleClick=(position)=>{
-        if(board[position] || winner){
+        if(current.board[position] || winner){
             return;
         }
 
-        setBoard((prev)=>{
-            return prev.map((updatedState,pos)=>{
+        setHistory((prev)=>{
+        const last=prev[prev.length-1];
+        const newBoard=
+          last.board.map((updatedState,pos)=>{
                 if(pos===position)
-                    return nextPlayer ? 'X':'O';
+                    return last.isNext ? 'X':'O';
                 return updatedState;
             });
+            return prev.concat({board:newBoard,isNext:!last.isNext});
         });
     
-    seNextPlayer((prev)=>!prev);
+    setCurrentMove(prev=>prev+1);
         
     };
 
@@ -34,7 +41,7 @@ const App=()=>{
   <div className='app'>
     <h1>Tic Tac Toe</h1>
     <h3>{message}</h3>
-    <Board board={board} hadnleClick={hadnleClick} />
+    <Board board={current.board} hadnleClick={hadnleClick} />
   </div>
   );
 }
